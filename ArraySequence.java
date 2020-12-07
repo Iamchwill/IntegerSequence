12ffa5916aa0f10ffa9763191e73714136b9a9cd
iamchwill <wchen20@stuy.edu>
2020-12-07 13:51:29 -0500

    Added needed methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -7,4 +7,24 @@ public class ArraySequence implements IntegerSequence{
     data = other;
     currentIndex = 0;
   }
+
+  public void reset() {
+    currentIndex = 0;
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public boolean hasNext(){
+    return currentIndex < data.length;
+  }
+
+  public int next(){
+    if (hasNext()){
+      currentIndex++;
+      return data[currentIndex - 1];
+    }
+    throw new NoSuchElementException();
+  }
 }
