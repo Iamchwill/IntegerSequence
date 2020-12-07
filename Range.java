c6fad80702e85671810b5a237f540aecc1f9c1e7
iamchwill <wchen20@stuy.edu>
2020-12-06 21:14:17 -0500

    Create Range. Added varibles and constructor.

---
+++ Range.java
@@ -0,0 +1,10 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+  private int start,end,current;
+
+  public Range(int first, int last){
+    start = first;
+    end = last;
+    current = first;
+  }
+}
